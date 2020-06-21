b2b9c27749eca34d1ecbba326b9f9738e90ae7a4
Shaun Simmons <shaun@shaun.pub>
2020-06-21 03:42:52 -0400

    Change package name

--- example.ts
+++ example.ts
@@ -1,18 +1,18 @@
-import { EventDispatcher, EventListener } from 'ivi-ts-event-dispatcher';
+import { EventDispatcher, EventListener } from '@indelible-vi/ts-event-dispatcher'
 
-const dispatcher = new EventDispatcher();
+const dispatcher = new EventDispatcher()
 
 // Set up an event listener.
 const listener: EventListener = (data, ev) => {
   // ... do something ...
-  sendWelcomeEmail(data.user);
+  sendWelcomeEmail(data.user)
 
   // Optionally, stop next listeners from being called.
-  ev.stopPropagation();
+  ev.stopPropagation()
 }
 
 // Register it in the dispatcher
-dispatcher.addListener("user.created", listener);
+dispatcher.addListener('user.created', listener)
 
 // Dispatch the event
-dispatcher.dispatch("user.created", { user });
+dispatcher.dispatch('user.created', { user })
