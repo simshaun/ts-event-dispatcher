19f2652f89792fb9c9b4bb7077eb04a5ba6683a0
Shaun Simmons <shaun@shaun.pub>
2020-12-01 02:42:22 -0500

    Better version bump verification

--- git-hook_verify-package-version-bump.js
+++ git-hook_verify-package-version-bump.js
@@ -2,17 +2,18 @@ const execa = require('execa')
 const proc = require('process')
 
 ;(async () => {
-  const versionBumpError = 'Expected version bump in package.json'
+  const versionBumpError = 'Expected version bump in package.json.'
 
-  const { stdout: status } = await execa('git status --porcelain=v1')
-  if (status.indexOf('M package.json') === -1) {
-    console.error(versionBumpError)
+  const { stdout: status } = await execa('git diff --staged --name-only')
+  if (!/^package\.json$/m.test(status)) {
+    console.error(`${versionBumpError} — git does not indicate package.json was modified`)
     proc.exit(1)
   }
 
   const { stdout: diff } = await execa('git diff --unified=0 package.json')
-  if (diff.indexOf('-  "version":') === -1 || diff.indexOf('+  "version":') === -1) {
-    console.error(versionBumpError)
+  // Not a 100% perfect solution due to small risk of false positives. However, it's good enough.
+  if (!/"version": "(\d+)\.(\d+)\.(\d+)"/.test(diff)) {
+    console.error(`${versionBumpError} — git does not indicate "version" was changed`)
     proc.exit(1)
   }
 })()
